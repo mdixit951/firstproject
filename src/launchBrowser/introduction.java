9a5fc8caf763fcafb98ff4d66526ca1ba95e92a5
bhuvan <bhuvandixit9@gmail.com>
2023-12-31 02:58:40 +0530

    first commit for the project

---
+++ src/launchBrowser/introduction.java
@@ -0,0 +1,16 @@
+package launchBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class introduction {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		
+	
+	}
+
+}
